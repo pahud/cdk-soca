7a48eaac931669e82cbf36a2f3a67036899e76a2
Pahud <pahudnet@gmail.com>
2020-10-04 11:44:52 +0800

    add projenyarnupgrade workflow

--- .projenrc.js
+++ .projenrc.js
@@ -1,12 +1,13 @@
 const {
-  ConstructLibraryAws,
+  AwsCdkConstructLibrary,
+  GithubWorkflow,
 } = require('projen');
 
 const AWS_CDK_LATEST_RELEASE = '1.63.0';
 const PROJECT_NAME = 'cdk-soca';
 const PROJECT_DESCRIPTION = 'cdk-soca is an AWS CDK construct library that allows you to create the Scale-Out Computing on AWS with AWS CDK in TypeScript or Python';
 
-const project = new ConstructLibraryAws({
+const project = new AwsCdkConstructLibrary({
   name: PROJECT_NAME,
   description: PROJECT_DESCRIPTION,
   repository: 'https://github.com/pahud/cdk-soca.git',
@@ -27,7 +28,7 @@ const project = new ConstructLibraryAws({
   },
 
   // creates PRs for projen upgrades
-  projenUpgradeSecret: 'AUTOMATION_GITHUB_TOKEN',
+  //projenUpgradeSecret: 'AUTOMATION_GITHUB_TOKEN',
 
   cdkVersion: AWS_CDK_LATEST_RELEASE,
   cdkDependencies: [
@@ -43,6 +44,49 @@ const project = new ConstructLibraryAws({
     module: 'cdk_soca'
   }
 });
+
+const workflow = new GithubWorkflow(project, 'ProjenYarnUpgrade');
+
+workflow.on({
+  schedule: [{
+    cron: '0 6 * * *'
+  }], // 6am every day
+  workflow_dispatch: {}, // allow manual triggering
+});
+
+workflow.addJobs({
+  upgrade: {
+    'runs-on': 'ubuntu-latest',
+    'steps': [
+      ...project.workflowBootstrapSteps,
+
+      // yarn upgrade
+      {
+        run: `yarn upgrade`
+      },
+
+      // upgrade projen
+      {
+        run: `yarn projen:upgrade`
+      },
+
+      // submit a PR
+      {
+        name: 'Create Pull Request',
+        uses: 'peter-evans/create-pull-request@v3',
+        with: {
+          'token': '${{ secrets.AUTOMATION_GITHUB_TOKEN }}',
+          'commit-message': 'chore: upgrade projen',
+          'branch': 'auto/projen-upgrade',
+          'title': 'chore: upgrade projen and yarn',
+          'body': 'This PR upgrades projen and yarn upgrade to the latest version',
+        }
+      },
+    ],
+  },
+});
+
+
 
 project.mergify.addRule({
   name: 'Merge approved pull requests with auto-merge label if CI passes',
@@ -60,6 +104,8 @@ project.mergify.addRule({
     },
   },
 });
+
+
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'images', 'yarn-error.log'];
 project.npmignore.exclude(...common_exclude);
