1432404ba76a8fd31a428efea3a66d6d89a81d76
Pahud <pahudnet@gmail.com>
2021-05-22 11:51:47 +0800

    deprecate projen-automate-it

--- .projenrc.js
+++ .projenrc.js
@@ -3,7 +3,7 @@ const { AwsCdkConstructLibrary } = require('projen');
 const AWS_CDK_LATEST_RELEASE = '1.77.0';
 const PROJECT_NAME = 'cdk-soca';
 const PROJECT_DESCRIPTION = 'cdk-soca is an AWS CDK construct library that allows you to create the Scale-Out Computing on AWS with AWS CDK in TypeScript or Python';
-const AUTOMATION_TOKEN = 'AUTOMATION_GITHUB_TOKEN';
+// const AUTOMATION_TOKEN = 'AUTOMATION_GITHUB_TOKEN';
 
 const project = new AwsCdkConstructLibrary({
   name: PROJECT_NAME,
@@ -45,45 +45,45 @@ const project = new AwsCdkConstructLibrary({
   },
 });
 
-// create a custom projen and yarn upgrade workflow
-workflow = project.github.addWorkflow('ProjenYarnUpgrade');
+// // create a custom projen and yarn upgrade workflow
+// workflow = project.github.addWorkflow('ProjenYarnUpgrade');
 
-workflow.on({
-  schedule: [{
-    cron: '11 0 * * *',
-  }], // 0:11am every day
-  workflow_dispatch: {}, // allow manual triggering
-});
+// workflow.on({
+//   schedule: [{
+//     cron: '11 0 * * *',
+//   }], // 0:11am every day
+//   workflow_dispatch: {}, // allow manual triggering
+// });
 
-workflow.addJobs({
-  upgrade: {
-    'runs-on': 'ubuntu-latest',
-    'steps': [
-      { uses: 'actions/checkout@v2' },
-      {
-        uses: 'actions/setup-node@v1',
-        with: {
-          'node-version': '10.17.0',
-        },
-      },
-      { run: 'yarn upgrade' },
-      { run: 'yarn projen:upgrade' },
-      // submit a PR
-      {
-        name: 'Create Pull Request',
-        uses: 'peter-evans/create-pull-request@v3',
-        with: {
-          'token': '${{ secrets.' + AUTOMATION_TOKEN + ' }}',
-          'commit-message': 'chore: upgrade projen',
-          'branch': 'auto/projen-upgrade',
-          'title': 'chore: upgrade projen and yarn',
-          'body': 'This PR upgrades projen and yarn upgrade to the latest version',
-          'labels': 'auto-merge',
-        },
-      },
-    ],
-  },
-});
+// workflow.addJobs({
+//   upgrade: {
+//     'runs-on': 'ubuntu-latest',
+//     'steps': [
+//       { uses: 'actions/checkout@v2' },
+//       {
+//         uses: 'actions/setup-node@v1',
+//         with: {
+//           'node-version': '10.17.0',
+//         },
+//       },
+//       { run: 'yarn upgrade' },
+//       { run: 'yarn projen:upgrade' },
+//       // submit a PR
+//       {
+//         name: 'Create Pull Request',
+//         uses: 'peter-evans/create-pull-request@v3',
+//         with: {
+//           'token': '${{ secrets.' + AUTOMATION_TOKEN + ' }}',
+//           'commit-message': 'chore: upgrade projen',
+//           'branch': 'auto/projen-upgrade',
+//           'title': 'chore: upgrade projen and yarn',
+//           'body': 'This PR upgrades projen and yarn upgrade to the latest version',
+//           'labels': 'auto-merge',
+//         },
+//       },
+//     ],
+//   },
+// });
 
 
 const common_exclude = ['cdk.out', 'cdk.context.json', 'images', 'yarn-error.log', '.devcontainer.json'];
